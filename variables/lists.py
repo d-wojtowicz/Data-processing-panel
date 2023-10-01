a2b1b5b1678d1c5f9b10916bd6a3ca1fde1359df
Dominik Wojtowicz <dwojtowicz1@o2.pl>
2023-10-01 13:37:13 +0200

    add: Constant values & operation enumerators

---
+++ variables/lists.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+
+# Data reader
+seaborn_libraries = sns.get_dataset_names()
+sklearn_libraries = ["iris", "digits", "wine", "diabetes", "breast_cancer"]
+
+# Pandas extension
+numeric_types = ['int', 'int64', 'float', 'float64']
+conditions_list = ['less', 'less than or equal', 'equal', 'greater than or equal', 'greater', '<', '<=', '==', '>=', '>']
+dtypes_list = ['category', 'object']
+
