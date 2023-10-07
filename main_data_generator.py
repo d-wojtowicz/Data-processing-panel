a670a65b1c8602de5fe45bcc7955d41400d045e3
Dominik Wojtowicz <dwojtowicz1@o2.pl>
2023-10-07 20:22:09 +0200

    add: export_to_csv, export_to_excel refactor: few small changes

--- main_data_generator.py
+++ main_data_generator.py
@@ -7,10 +7,8 @@ from utils.data_exporter import *
 from utils.pandas_extension import *
 
 # TESTING GENERATOR FUNCTIONS
-genMakingMeasure = calc.fullMeasure(generate_dataframe)
-dfMakingMeasure = calc.fullMeasure(gen_to_df)
-txtExportMeasure = calc.fullMeasure(export_to_txt) 
-jsonExportMeasure = calc.fullMeasure(export_to_json)
+genMakingMeasure = calc.fullMeasure(generate_dataframe) # Turned on time & storage measurement of Generating Dataframe
+dfMakingMeasure = calc.fullMeasure(gen_to_df)           # Turned on time & storage measurement of Parsing types from Gen to Df
 
 gen_of_small_df = genMakingMeasure(10, 25)       # e.g. Generator creating objects (based on the specified pattern)
 #gen_of_medium_df = genMakingMeasure(10, 500)
@@ -22,10 +20,24 @@ df_small = dfMakingMeasure(gen_of_small_df)
 #df_big = dfMakingMeasure(gen_of_big_df)
 #df_large = dfMakingMeasure(gen_of_large_df)
 
-small_df_in_generator =  df_to_gen(df_small)    # e.g. Generator storing object (compressing the storage)
+small_df_in_generator = df_to_gen(df_small)    # e.g. Generator storing object (compressing the storage)
+#medium_df_in_generator = df_to_gen(df_medium)
+#big_df_in_generator = df_to_gen(df_big)
+#large_df_in_generator = df_to_gen(df_large)
+
+
+
+# TESTING EXPORTS
+txtExportMeasure = calc.fullMeasure(export_to_txt) 
+jsonExportMeasure = calc.fullMeasure(export_to_json)
+csvExportMeasure = calc.fullMeasure(export_to_csv)
+xlsxExportMeasure = calc.fullMeasure(export_to_xlsx)
 
 #txtExportMeasure(df_small, "as_df")
 #txtExportMeasure(small_df_in_generator, "as_gen")
-
-jsonExportMeasure(df_small, "as_df")
-jsonExportMeasure(small_df_in_generator, "as_gen")
+#jsonExportMeasure(df_small, "as_df")
+#jsonExportMeasure(small_df_in_generator, "as_gen")
+#csvExportMeasure(df_small, "as_df")
+#csvExportMeasure(small_df_in_generator, "as_gen")
+#xlsxExportMeasure(df_small, "as_df")
+#xlsxExportMeasure(small_df_in_generator, "as_gen")
--- utils/data_exporter.py
+++ utils/data_exporter.py
@@ -3,7 +3,9 @@
 
 import os
 
+import csv
 import json
+import openpyxl
 import pandas as pd
 
 from datetime import date, datetime
@@ -16,42 +18,82 @@ relative_path = "files"
 files_path = os.path.join(project_path, relative_path)
 
 def export_to_txt(dataset: Union[pd.DataFrame, GeneratorType], file_name: str) -> None:
-    todays_date = str(datetime.now().strftime("%d%m%Y_%H%M%S"))
-    file_name = todays_date + "_" + file_name + ".txt"
+    full_path = filepath_creator(file_name, "txt")
 
-    full_path = os.path.join(files_path, file_name)
-    
-    with open(full_path, "w") as f:
+    with open(full_path, "w") as Txt:
         if type(dataset) == pd.DataFrame:
-            f.write(value_separator.join(map(str, dataset.columns)) + row_separator + "\n")
+            Txt.write(value_separator.join(map(str, dataset.columns)) + row_separator + "\n")
             for _, row in dataset.iterrows():
                 row_str = value_separator.join(map(str, row))
-                f.write(row_str + row_separator + "\n")
+                Txt.write(row_str + row_separator + "\n")
         elif type(dataset) == GeneratorType:
             columns_displayed = False
             for row in dataset:
                 if not columns_displayed:
                     column_str = value_separator.join(map(str, row.keys()))
-                    f.write(column_str + row_separator + "\n")
+                    Txt.write(column_str + row_separator + "\n")
                     columns_displayed = True
                 
                 row_str = value_separator.join(map(str, row))
-                f.write(row_str + row_separator + "\n")
+                Txt.write(row_str + row_separator + "\n")
         else:
             raise Exception("Unwanted input data types.")
     
 
 def export_to_json(dataset: Union[pd.DataFrame, GeneratorType], file_name: str) -> None:
-    todays_date = str(datetime.now().strftime("%d%m%Y_%H%M%S"))
-    file_name = todays_date + "_" + file_name + ".json"
-
-    full_path = os.path.join(files_path, file_name)
-
-    with open(full_path, "w") as f:
+    full_path = filepath_creator(file_name, "json")
+    
+    with open(full_path, "w") as Json:
         if type(dataset) == pd.DataFrame:
             df = dataset.to_json(orient="records", lines=True)
-            f.write(df)
+            Json.write(df)
         elif type(dataset) == GeneratorType:
             for row in dataset:
-                f.write(json.dumps(row.to_dict()) + "\n")
+                Json.write(json.dumps(row.to_dict()) + "\n")
+        else:
+            raise Exception("Unwanted input data types.")
+        
+def export_to_csv(dataset: Union[pd.DataFrame, GeneratorType], file_name: str) -> None:
+    full_path = filepath_creator(file_name, "csv")
+    
+    with open(full_path, "w", newline="") as Csv:
+        if type(dataset) == pd.DataFrame:
+            dataset.to_csv(Csv, index=False, sep=value_separator, header=True)
+        elif type(dataset) == GeneratorType:
+            csv_writer = csv.writer(Csv, delimiter=value_separator)
+            
+            column_displayed = False
+            for row in dataset:
+                if not column_displayed:
+                    csv_writer.writerow(row.keys())
+                    column_displayed = True
+                csv_writer.writerow(row)
+        else:
+            raise Exception("Unwanted input data types.")
 
+def export_to_xlsx(dataset: Union[pd.DataFrame, GeneratorType], file_name: str) -> None:
+    full_path = filepath_creator(file_name, "xlsx")
+
+    if type(dataset) == pd.DataFrame:
+        dataset.to_excel(full_path, index=False, header=True)
+    elif type(dataset) == GeneratorType:
+        excelFile = openpyxl.Workbook()
+        excelSheet = excelFile.active
+
+        column_displayed = False
+        for row in dataset:
+            if not column_displayed:
+                excelSheet.append(row.keys().tolist())
+                column_displayed = True
+            excelSheet.append(row.tolist())
+        excelFile.save(full_path)
+    else:
+        raise Exception("Unwanted input data types.")
+        
+
+def filepath_creator(file_name: str, extension: str) -> str:
+    todays_date = str(datetime.now().strftime("%d%m%Y_%H%M%S"))
+    file_name = todays_date + "_" + file_name + "." + extension
+    full_path = os.path.join(files_path, file_name)
+
+    return full_path
