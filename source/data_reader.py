7399275be9c76c11734137ab702016add24fe04a
Gruby3773 <dwojtowicz1@o2.pl>
2023-10-22 23:48:51 +0200

    refactor: dataReaders are now compatible with each other and written in OOP (object-oriented programming)

--- source/data_reader.py
+++ source/data_reader.py
@@ -1,45 +1,35 @@
-# !TODO: Rewrite all input reads and result returns to operate on the generators from the dataframe rather than the dataframe!!!
-# Details: At the most detailed moments, such as reading by columns or chunks or tuples (etc.),
-# I should read not from the finished dataframe object but from the object passed through df_to_gen
-# (i.e. read from the generator, not the dataframe)
-
-# TODO: Reading datasets individually (e.g. via input files)
-
-import csv
-
-import seaborn as sns
 import pandas as pd
+from typing import Union
+from types import GeneratorType
 
 from variables.enumerators import *
 from variables.lists import *
-from source.df_seaborn_reader import read_df_from_seaborn
-from source.df_sklearn_reader import read_df_from_sklearn
-from source.df_individual_reader import read_df_from_input
-
+from source.df_seaborn_reader import DataSeabornReader
+from source.df_sklearn_reader import DataSklearnReader
+from source.df_individual_reader import DataIndividualReader
+ 
 class DataReader(object):
     def __init__(self, df_name: str, df_source: str, location_method: Enum = read_from.TOP, structure_method: Enum = read_by.NORMAL, limit: int = 1000, by_gen: bool = True):
         """ 'by_gen' parameter concerns only to the 'row_reader' & 'tuple_reader' & 'chunk_reader"""
-        self.df_name = df_name
-        self.df_source = df_source
+        self.dataset: Union[pd.DataFrame, GeneratorType] = None
 
-        self.location_method = location_method
-        self.structure_method = structure_method
-        self.limit = limit
-        self.by_gen = by_gen
+        self.df_name: str = df_name
+        self.df_source: str = df_source
 
-    def read_data(self) -> pd.DataFrame:
-        df = pd.DataFrame()
+        self.location_method: Enum = location_method
+        self.structure_method: Enum = structure_method
+        self.limit: int = limit
+        self.by_gen: bool = by_gen
 
+    def read_data(self) -> None:
         try:
             if self.df_source in ["sns", "seaborn"]:
                 if self.df_name in seaborn_libraries:
-                    df = read_df_from_seaborn(self.df_name, self.by_gen, self.location_method, self.structure_method, self.limit)
+                    self.dataset = DataSeabornReader(self.df_name, self.location_method, self.structure_method, self.limit, self.by_gen).dataset
             elif self.df_source in ["sklearn", "scikit-learn"]:
                 if self.df_name in sklearn_libraries:
-                    df = read_df_from_sklearn(self.df_name, self.by_gen, self.location_method, self.structure_method, self.limit)
+                    self.dataset = DataSklearnReader(self.df_name, self.location_method, self.structure_method, self.limit, self.by_gen).dataset
             elif self.df_source.endswith((".txt", ".csv", ".xlsx", ".json")):
-                df = read_df_from_input(self.df_source, self.by_gen, self.location_method) # Full data are readed
+                self.dataset = DataIndividualReader(self.df_source, self.location_method, by_gen=self.by_gen).dataset # Full data are readed
         except:
             raise Exception("The dataset could NOT be readed!")
-        
-        return pd.DataFrame(df)
