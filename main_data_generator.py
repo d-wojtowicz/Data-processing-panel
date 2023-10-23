1f2ba8dd4e7dc150a2f1f77d0be1526da818a200
Gruby3773 <dwojtowicz1@o2.pl>
2023-10-23 23:57:19 +0200

    refactor: dataGenerator is written in OOP

--- main_data_generator.py
+++ main_data_generator.py
@@ -1,12 +1,14 @@
+# TODO: Apply changes due to the OOP refactor of !DataReader and *DataManager
+
 import seaborn as sns
 import pandas as pd
 
-from utils.data_generator_by_gen import *
-from utils.not_generators.data_generator_by_obj import *
+from utils.not_generators.data_generator_by_obj import DataGenerator
+from utils.data_generator_by_gen import DataGenerator
 
 from utils.data_stats_calculator import *
-from utils.data_exporter import *
-from utils.pandas_extension import *
+from utils.data_exporter import DataExporter
+from utils.pandas_extension import DataManager, gen_to_df, df_to_gen
 
 
 if __name__ == "__main__":
@@ -15,7 +17,11 @@ if __name__ == "__main__":
     dfMakingMeasure = calc.fullMeasure(gen_to_df) # Turned on time & storage measurement of Parsing types from Gen to Df
 
     # TESTING CREATOR (FROM GEN) FUNCTIONS
-    dataMakingByGenMeasure = calc.fullMeasure(generate_dataframe_by_gen) # Turned on time & storage measurement of Generating Dataframe by generator
+    gen_small = DataGenerator(10, 25)
+    #gen_medium = DataGenerator(10, 500)
+    #gen_big = DataGenerator(15, 5000)
+    #gen_large = DataGenerator(20, 25000)
+    dataMakingByGenMeasure = calc.fullMeasure(gen_small.generate_dataframe_by_gen) # Turned on time & storage measurement of Generating Dataframe by generator
 
     # Creating Generators 
     gen_small = dataMakingByGenMeasure(10, 25)       # e.g. Generator creating objects (based on the specified pattern)
--- utils/data_generator_by_gen.py
+++ utils/data_generator_by_gen.py
@@ -3,40 +3,45 @@ import pandas as pd
 from random import randint, choice
 from variables.lists import data_types, letters
 
-def generate_dataframe_by_gen(number_of_cols: int, number_of_records: int):
-    def generate_dataframe_generator() -> pd.DataFrame:
-        col_names = ["Column" + str(i+1) for i in range(number_of_cols)]
-        col_types = [choice(data_types) for _ in range(number_of_cols)]
-        col_dict = dict(zip(col_names, col_types))
+class DataGenerator(object):
+    def __init__(self, number_of_cols: int, number_of_records: int):
+        self.number_of_cols: int = number_of_cols
+        self.number_of_records: int = number_of_records
 
+    def generate_dataframe_by_gen(self):
+        def generate_dataframe_generator() -> pd.DataFrame:
+            col_names = ["Column" + str(i+1) for i in range(self.number_of_cols)]
+            col_types = [choice(data_types) for _ in range(self.number_of_cols)]
+            col_dict = dict(zip(col_names, col_types))
+
+            for _ in range(self.number_of_records):
+                row_data = {col_name: next(self.generate_column(1, col_type)) for col_name, col_type in col_dict.items()}
+                yield pd.DataFrame(row_data, index=[0])
+
+        return generate_dataframe_generator()
+
+    def generate_column(self, number_of_records: int, data_type: str):
+        if data_type == "number":
+            yield from self.generate_number_column(1, 100, number_of_records)
+        elif data_type == "text":
+            yield from self.generate_text_column(5, 10, number_of_records)
+        elif data_type == "char":
+            yield from self.generate_char_column(number_of_records)
+
+
+    def generate_number_column(self, min_value: int, max_value: int, number_of_records: int):
         for _ in range(number_of_records):
-            row_data = {col_name: next(generate_column(1, col_type)) for col_name, col_type in col_dict.items()}
-            yield pd.DataFrame(row_data, index=[0])
+            yield randint(min_value, max_value)
 
-    return generate_dataframe_generator()
+    def generate_text_column(self, min_length: int, max_length: int, number_of_records: int):
+        for _ in range(number_of_records):
+            yield "".join(self.generate_text_to_single_record(min_length,max_length))
 
-def generate_column(number_of_records: int, data_type: str):
-    if data_type == "number":
-        yield from generate_number_column(1, 100, number_of_records)
-    elif data_type == "text":
-        yield from generate_text_column(5, 10, number_of_records)
-    elif data_type == "char":
-        yield from generate_char_column(number_of_records)
+    def generate_char_column(self, number_of_records: int):
+        for _ in range(number_of_records):
+            yield chr(randint(65,90))
 
 
-def generate_number_column(min_value: int, max_value: int, number_of_records: int):
-    for _ in range(number_of_records):
-        yield randint(min_value, max_value)
-
-def generate_text_column(min_length: int, max_length: int, number_of_records: int):
-    for _ in range(number_of_records):
-        yield "".join(generate_text_to_single_record(min_length,max_length))
-
-def generate_char_column(number_of_records: int):
-    for _ in range(number_of_records):
-        yield chr(randint(65,90))
-
-
-def generate_text_to_single_record(min_length: int, max_length: int):
-    for _ in range(randint(min_length, max_length)):
-        yield choice(letters)
+    def generate_text_to_single_record(self, min_length: int, max_length: int):
+        for _ in range(randint(min_length, max_length)):
+            yield choice(letters)
