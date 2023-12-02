b9e37efa9845fc82f2922ab61b2c597a84147a03
Gruby3773 <dwojtowicz1@o2.pl>
2023-12-02 21:19:55 +0100

    add: new trace size and time measurement tool

--- gen_app/utils/data_stats_calculator.py
+++ gen_app/utils/data_stats_calculator.py
@@ -4,6 +4,18 @@ import tracemalloc
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class calc:
+    # UNIVERSAL METHOD
+    @staticmethod
+    def timeMeasure(func):
+        def wrapper(*args, **kwargs):
+            t_start = time.process_time()
+            result = func(*args, **kwargs)
+            t_end = time.process_time()
+            print(f"{func}: (Czas: {t_end - t_start} [s])")
+            return result
+        return wrapper
+
+    # SINGLE OBJECT METHODS
     @staticmethod
     def fullMeasure(func):
         def wrapper(*args, **kwargs):
@@ -15,16 +27,6 @@ class calc:
         return wrapper
     
     @staticmethod
-    def timeMeasure(func):
-        def wrapper(*args, **kwargs):
-            t_start = time.process_time()
-            result = func(*args, **kwargs)
-            t_end = time.process_time()
-            print(f"{func}: (Czas: {t_end - t_start} [s])")
-            return result
-        return wrapper
-
-    @staticmethod
     def sizeMeasure(obj):
         def wrapper(*args, **kwargs):
             result = obj(*args, **kwargs)
@@ -32,11 +34,25 @@ class calc:
             return result
         return wrapper
     
+    # MULTI OBJECT METHODS
     @staticmethod
-    def traceSizeMeasure(obj):
+    def traceFullMeasure(func):
+        def wrapper(*args, **kwargs):
+            t_start = time.process_time()
+            tracemalloc.start()
+            result = func(*args, **kwargs)
+            curr_mem, peak_mem = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
+            t_end = time.process_time()
+
+            return result, t_end - t_start, curr_mem, peak_mem
+        return wrapper
+
+    @staticmethod
+    def traceSizeMeasure(func):
         def wrapper(*args, **kwargs):
             tracemalloc.start()
-            result = obj(*args, **kwargs)
+            result = func(*args, **kwargs)
             current, peak = tracemalloc.get_traced_memory()
             tracemalloc.stop()
             return result, current, peak
